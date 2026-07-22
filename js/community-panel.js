6c22ed6b48e2490dab7177a625a3e95db0685cb8
Otto Eric <70889383+Otto200@users.noreply.github.com>
2026-07-22 20:54:32 +1000

    Create community-panel.js

---
+++ js/community-panel.js
@@ -0,0 +1,8 @@
+// Initialization specific to the Paid Premium PNG Membership module
+document.addEventListener("DOMContentLoaded", () => {
+    const communityPanel = document.getElementById("tab-community");
+    
+    if (communityPanel) {
+        console.log("BankBugs | FX VIP Premium Community Module initialized successfully.");
+    }
+});
